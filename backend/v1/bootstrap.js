77a71e4e166124aa768ffb35dd3ad7a47b1fd7c6
Andre Lima <andrelimamail@gmail.com>
2021-03-29 20:38:54 +0100

    removed useless files

--- backend/v1/bootstrap.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const usd = require('./usd/api');
-var app = express();
-
-usd.api(app);
-
-app.listen(8080, function(){
-    console.log("Servidor ativo no porto 8080");
-});
